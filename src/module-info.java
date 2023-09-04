fc7921479f1a6673d1451eb82571b00fe704ca40
Miguel <klausmein@hotmail.com>
2023-09-04 11:51:40 -0600

    Comienzo del proyecto

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author klaus
+ *
+ */
+module Test {
+}
---
+++ src/testUno/PrimerPrueba.java
@@ -0,0 +1,10 @@
+package testUno;
+
+public class PrimerPrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
