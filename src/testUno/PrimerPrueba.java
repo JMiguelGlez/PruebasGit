32b9915c233f8a58aea87fe935ecd8b1d355f4f6
JMiguelGlez <143457127+JMiguelGlez@users.noreply.github.com>
2023-09-04 14:17:26 -0600

    Update from gitHub PrimerPrueba.java

--- src/testUno/PrimerPrueba.java
+++ src/testUno/PrimerPrueba.java
@@ -4,7 +4,8 @@ public class PrimerPrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String var = "Esta es la primer prueba";
+		String change = "Cambio hecho desde gitHub";
 	}
 
 }
